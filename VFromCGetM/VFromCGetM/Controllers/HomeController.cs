9d77195e5b888c05c026555f3d8c945b41f9b967
paul007008 <hello paul>
2018-06-20 09:55:30 +0800

    通过viewmode获取mode数据

--- VFromCGetM/VFromCGetM/Controllers/HomeController.cs
+++ VFromCGetM/VFromCGetM/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VFromCGetM.Models;
-
+using VFromCGetM.ViewModels;
 
 namespace VFromCGetM.Controllers
 {
@@ -27,7 +27,26 @@ namespace VFromCGetM.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            Employee emp = new Employee();
+                emp.FirstName = "Sukesh";
+                emp.LastName = "Marla";
+                 emp.Salary = 20000;
+             
+       EmployeeViewModel vmEmp = new EmployeeViewModel();
+                vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
+                 vmEmp.Salary = emp.Salary.ToString("C");
+                 if (emp.Salary > 15000)
+                      {
+                          vmEmp.SalaryColor = "yellow";
+                      }
+                else
+       {
+                        vmEmp.SalaryColor = "green";
+                    }
+            
+   vmEmp.UserName = "Admin";
+   
+      return View("About", vmEmp);
 
             return View();
         }
